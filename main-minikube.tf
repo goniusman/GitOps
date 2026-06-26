cc3cffb56b0c40cb1c5142f14ae748e0a9e488f4
goniusman <goniusman400@gmail.com>
2026-06-26 18:17:22 +0600

    fixed pvc

--- main-minikube.tf
+++ main-minikube.tf
@@ -25,7 +25,7 @@ resource "minikube_cluster" "my_cluster" {
   cluster_name = "minikube-iac"
   cpus         = "4"
   memory       = "8192mb"
-  addons       = ["ingress", "dashboard", "metrics-server"]
+  addons       = ["ingress", "dashboard", "metrics-server", "storage-provisioner"]
 }
 
 # Connect providers directly to the cluster attributes
@@ -58,80 +58,77 @@ resource "helm_release" "argocd" {
   version    = "9.5.14"
 }
 
-# --- POSTGRESQL STORAGE ---
-resource "kubernetes_persistent_volume" "postgres_pv" {
-  metadata { name = "postgres-pv" }
-  spec {
-    capacity           = { storage = "5Gi" }
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    persistent_volume_source {
-      host_path {
-        path = "/mnt/data/postgres"
-      }
-    }
-  }
-}
+# # --- POSTGRESQL STORAGE ---
+# resource "kubernetes_persistent_volume" "postgres_pv" {
+#   metadata { name = "postgres-pv" }
+#   spec {
+#     capacity           = { storage = "5Gi" }
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     persistent_volume_source {
+#       host_path {
+#         path = "/mnt/data/postgres"
+#       }
+#     }
+#   }
+# }
+# resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+#   metadata { name = "postgres-pvc" }
+#   spec {
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     resources { requests = { storage = "5Gi" } }
+#     volume_name        = kubernetes_persistent_volume.postgres_pv.metadata[0].name
+#   }
+# }
+# # --- MONGODB STORAGE ---
+# resource "kubernetes_persistent_volume" "mongodb_pv" {
+#   metadata { name = "mongodb-pv" }
+#   spec {
+#     capacity           = { storage = "10Gi" }
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     persistent_volume_source {
+#       host_path {
+#         path = "/mnt/data/mongodb"
+#       }
+#     }
+#   }
+# }
+# resource "kubernetes_persistent_volume_claim" "mongodb_pvc" {
+#   metadata { name = "mongodb-pvc" }
+#   spec {
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     resources { requests = { storage = "10Gi" } }
+#     volume_name        = kubernetes_persistent_volume.mongodb_pv.metadata[0].name
+#   }
+# }
+# # --- REDIS STORAGE ---
+# resource "kubernetes_persistent_volume" "redis_pv" {
+#   metadata { name = "redis-pv" }
+#   spec {
+#     capacity           = { storage = "2Gi" }
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     persistent_volume_source {
+#       host_path {
+#         path = "/mnt/data/redis"
+#       }
+#     }
+#   }
+# }
+# resource "kubernetes_persistent_volume_claim" "redis_pvc" {
+#   metadata { name = "redis-pvc" }
+#   spec {
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "manual"
+#     resources { requests = { storage = "2Gi" } }
+#     volume_name        = kubernetes_persistent_volume.redis_pv.metadata[0].name
+#   }
+# }
 
-resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
-  metadata { name = "postgres-pvc" }
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    resources { requests = { storage = "5Gi" } }
-    volume_name        = kubernetes_persistent_volume.postgres_pv.metadata[0].name
-  }
-}
 
-# --- MONGODB STORAGE ---
-resource "kubernetes_persistent_volume" "mongodb_pv" {
-  metadata { name = "mongodb-pv" }
-  spec {
-    capacity           = { storage = "10Gi" }
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    persistent_volume_source {
-      host_path {
-        path = "/mnt/data/mongodb"
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "mongodb_pvc" {
-  metadata { name = "mongodb-pvc" }
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    resources { requests = { storage = "10Gi" } }
-    volume_name        = kubernetes_persistent_volume.mongodb_pv.metadata[0].name
-  }
-}
-
-# --- REDIS STORAGE ---
-resource "kubernetes_persistent_volume" "redis_pv" {
-  metadata { name = "redis-pv" }
-  spec {
-    capacity           = { storage = "2Gi" }
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    persistent_volume_source {
-      host_path {
-        path = "/mnt/data/redis"
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "redis_pvc" {
-  metadata { name = "redis-pvc" }
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "manual"
-    resources { requests = { storage = "2Gi" } }
-    volume_name        = kubernetes_persistent_volume.redis_pv.metadata[0].name
-  }
-}
 
 # 3. Bootstrap Application Infrastructure using Native PowerShell local-exec
 resource "null_resource" "bootstrap_argocd_apps" {
